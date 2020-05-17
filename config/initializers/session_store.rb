a9bdd552bde6b9c90375548db5cd0739a699babd
Yung Cher Ho <ycho527@hotmail.com>
2020-05-17 23:52:32 +0800

    Added name to User Model and configured registrations

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "travel-uncharted.herokuapp.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "travel-uncharted.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
